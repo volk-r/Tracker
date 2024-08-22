f6a86185675bf4011e1347a71d8f2f4a9f021298
Roman Romanov <rromanov.vk@gmail.com>
2024-08-23 00:38:28 +0500

    init

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Roman Romanov on 22.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
