f2ff81dde7c163106848aa9f2f31e690f5c61978
Roman Romanov <rromanov.vk@gmail.com>
2024-08-25 00:12:10 +0500

    adding AppColorSettings, adding support hex for UIColor, adding LaunchScreen

---
+++ Tracker/AppSettings/AppColorSettings.swift
@@ -0,0 +1,12 @@
+//
+//  AppColorSettings.swift
+//  Tracker
+//
+//  Created by Roman Romanov on 24.08.2024.
+//
+
+import UIKit
+
+enum AppColorSettings {
+    static let launchScreenBackgroundColor = UIColor(hexString: "#3772E7")
+}
