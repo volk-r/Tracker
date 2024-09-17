c7ad103fc81a4afca8dd7f3d48cd3f9ffbd2dd8e
Roman Romanov <rromanov.vk@gmail.com>
2024-09-17 22:01:02 +0500

    adding CoreData Model

--- Tracker/Presentation/Schedule/Models/WeekDay.swift
+++ Tracker/Presentation/Schedule/Models/WeekDay.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum WeekDay: Int, CaseIterable, Comparable {
+enum WeekDay: Int, CaseIterable, Comparable, Codable {
     case monday = 2
     case tuesday = 3
     case wednesday = 4
