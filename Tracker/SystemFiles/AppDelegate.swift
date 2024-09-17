fb3326af1073a2b7d874580b15dde2b87c44dbdd
Roman Romanov <rromanov.vk@gmail.com>
2024-09-17 23:04:55 +0500

    adding persistentContainer

--- Tracker/SystemFiles/AppDelegate.swift
+++ Tracker/SystemFiles/AppDelegate.swift
@@ -6,9 +6,20 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores { description, error in
+            if let error = error as NSError? {
+                preconditionFailure("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
