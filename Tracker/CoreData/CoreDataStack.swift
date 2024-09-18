66a15af199e57f4facfacbf1fa914445cf6e4344
Roman Romanov <rromanov.vk@gmail.com>
2024-09-18 23:37:57 +0500

    fixed CoreDataStack singletone

--- Tracker/CoreData/CoreDataStack.swift
+++ Tracker/CoreData/CoreDataStack.swift
@@ -31,7 +31,6 @@ final class CoreDataStack: CoreDataStackProtocol {
     }
     
     func saveContext() {
-        let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
                 try context.save()
